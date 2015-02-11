78c4118ec0140ed8461d374dc53a12354878791b
Tim Emiola <temiola@google.com>
2015-02-11 08:40:59 -0800

    Adds ApplicationDefaultCredentials
    ApplicationDefaultCredentials is a factory for FetchAuthTokenInteface; it gets
    a default instance of that class that is configured by the application
    environment.
    
    It determines what credentials to use according the rules described in
    
    https://developers.google.com/accounts/docs/application-default-credentials

---
+++ tests/JustAuth/ApplicationDefaultCredentialsTest.php
@@ -0,0 +1,170 @@
+<?php
+/*
+ * Copyright 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Google\Auth\Tests;
+
+use Google\Auth\ApplicationDefaultCredentials;
+use Google\Auth\GCECredentials;
+use Google\Auth\ServiceAccountCredentials;
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Stream\Stream;
+use GuzzleHttp\Subscriber\Mock;
+
+class ADCGetTest extends \PHPUnit_Framework_TestCase
+{
+  var $originalHome;
+
+  protected function setUp()
+  {
+    $this->originalHome = getenv('HOME');
+  }
+
+  protected function tearDown()
+  {
+    if ($this->originalHome != getenv('HOME')) {
+      putenv('HOME=' . $this->originalHome);
+    }
+    putenv(ServiceAccountCredentials::ENV_VAR);  // removes it from
+  }
+
+  /**
+   * @expectedException DomainException
+   */
+  public function testIsFailsEnvSpecifiesNonExistentFile()
+  {
+    $keyFile = __DIR__ . '/fixtures' . '/does-not-exist-private.json';
+    putenv(ServiceAccountCredentials::ENV_VAR . '=' . $keyFile);
+    ApplicationDefaultCredentials::get('a scope');
+  }
+
+  public function testLoadsOKIfEnvSpecifiedIsValid()
+  {
+    $keyFile = __DIR__ . '/fixtures' . '/private.json';
+    putenv(ServiceAccountCredentials::ENV_VAR . '=' . $keyFile);
+    $this->assertNotNull(ApplicationDefaultCredentials::get('a scope'));
+  }
+
+  public function testLoadsDefaultFileIfPresentAndEnvVarIsNotSet()
+  {
+    putenv('HOME=' . __DIR__ . '/fixtures');
+    $this->assertNotNull(ApplicationDefaultCredentials::get('a scope'));
+  }
+
+  /**
+   * @expectedException DomainException
+   */
+  public function testFailsIfNotOnGceAndNoDefaultFileFound()
+  {
+    $client = new Client();
+    // simulate not being GCE by return 500
+    $client->getEmitter()->attach(new Mock([new Response(500)]));
+    ApplicationDefaultCredentials::get('a scope', $client);
+  }
+
+  public function testSuccedsIfNoDefaultFilesButIsOnGCE()
+  {
+    $client = new Client();
+    // simulate the response from GCE.
+    $wantedTokens = [
+        'access_token' => '1/abdef1234567890',
+        'expires_in' => '57',
+        'token_type' => 'Bearer',
+    ];
+    $jsonTokens = json_encode($wantedTokens);
+    $client = new Client();
+    $plugin = new Mock([
+        new Response(200, [GCECredentials::FLAVOR_HEADER => 'Google']),
+        new Response(200, [], Stream::factory($jsonTokens)),
+    ]);
+    $client->getEmitter()->attach($plugin);
+    $this->assertNotNull(
+        ApplicationDefaultCredentials::get('a scope', $client));
+  }
+}
+
+class ADCGetFetcherTest extends \PHPUnit_Framework_TestCase
+{
+  var $originalHome;
+
+  protected function setUp()
+  {
+    $this->originalHome = getenv('HOME');
+  }
+
+  protected function tearDown()
+  {
+    if ($this->originalHome != getenv('HOME')) {
+      putenv('HOME=' . $this->originalHome);
+    }
+    putenv(ServiceAccountCredentials::ENV_VAR);  // removes it from
+  }
+
+  /**
+   * @expectedException DomainException
+   */
+  public function testIsFailsEnvSpecifiesNonExistentFile()
+  {
+    $keyFile = __DIR__ . '/fixtures' . '/does-not-exist-private.json';
+    putenv(ServiceAccountCredentials::ENV_VAR . '=' . $keyFile);
+    ApplicationDefaultCredentials::getFetcher('a scope');
+  }
+
+  public function testLoadsOKIfEnvSpecifiedIsValid()
+  {
+    $keyFile = __DIR__ . '/fixtures' . '/private.json';
+    putenv(ServiceAccountCredentials::ENV_VAR . '=' . $keyFile);
+    $this->assertNotNull(ApplicationDefaultCredentials::getFetcher('a scope'));
+  }
+
+  public function testLoadsDefaultFileIfPresentAndEnvVarIsNotSet()
+  {
+    putenv('HOME=' . __DIR__ . '/fixtures');
+    $this->assertNotNull(ApplicationDefaultCredentials::getFetcher('a scope'));
+  }
+
+  /**
+   * @expectedException DomainException
+   */
+  public function testFailsIfNotOnGceAndNoDefaultFileFound()
+  {
+    $client = new Client();
+    // simulate not being GCE by return 500
+    $client->getEmitter()->attach(new Mock([new Response(500)]));
+    ApplicationDefaultCredentials::getFetcher('a scope', $client);
+  }
+
+  public function testSuccedsIfNoDefaultFilesButIsOnGCE()
+  {
+    $client = new Client();
+    // simulate the response from GCE.
+    $wantedTokens = [
+        'access_token' => '1/abdef1234567890',
+        'expires_in' => '57',
+        'token_type' => 'Bearer',
+    ];
+    $jsonTokens = json_encode($wantedTokens);
+    $client = new Client();
+    $plugin = new Mock([
+        new Response(200, [GCECredentials::FLAVOR_HEADER => 'Google']),
+        new Response(200, [], Stream::factory($jsonTokens)),
+    ]);
+    $client->getEmitter()->attach($plugin);
+    $this->assertNotNull(
+        ApplicationDefaultCredentials::getFetcher('a scope', $client));
+  }
+}
