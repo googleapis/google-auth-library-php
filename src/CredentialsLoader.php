cfb2b6887b069e60c117d061b1fe9ab23b1c4bb7
Stanley Cheung <stanleycheung@google.com>
2015-05-22 08:21:50 -0700

    add constant

--- src/CredentialsLoader.php
+++ src/CredentialsLoader.php
@@ -32,6 +32,7 @@ class CredentialsLoader implements FetchAuthTokenInterface
   const TOKEN_CREDENTIAL_URI = 'https://www.googleapis.com/oauth2/v3/token';
   const ENV_VAR = 'GOOGLE_APPLICATION_CREDENTIALS';
   const WELL_KNOWN_PATH = 'gcloud/application_default_credentials.json';
+  const NON_WINDOWS_WELL_KNOWN_PATH_BASE = '.config';
   const AUTH_METADATA_KEY = 'Authorization';
 
   private static function unableToReadEnv($cause)
@@ -97,7 +98,7 @@ class CredentialsLoader implements FetchAuthTokenInterface
     $rootEnv = self::isOnWindows() ? 'APPDATA' : 'HOME';
     $path = [getenv($rootEnv)];
     if (!self::isOnWindows()) {
-      $path[] = '.config';
+      $path[] = self::NON_WINDOWS_WELL_KNOWN_PATH_BASE;
     }
     $path[] = self::WELL_KNOWN_PATH;
     $path = join(DIRECTORY_SEPARATOR, $path);
