8057ceda3e324304a6443b82f1a56f52cd9e600d
Takashi Matsuo <tmatsuo@google.com>
2018-07-19 10:29:58 -0700

    Add a warning for 3 legged OAuth credentials (#199)
    * Add a warning for 3 legged OAuth credentials
    
    * CS fix

--- src/Credentials/UserRefreshCredentials.php
+++ src/Credentials/UserRefreshCredentials.php
@@ -80,6 +80,16 @@ class UserRefreshCredentials extends CredentialsLoader
             'scope' => $scope,
             'tokenCredentialUri' => self::TOKEN_CREDENTIAL_URI,
         ]);
+        trigger_error(
+            'Your application has authenticated using end user credentials '
+            . 'from Gooogle Cloud SDK. We recommend that most server '
+            . 'applications use service accounts instead. If your application '
+            . 'continues to use end user credentials from Cloud SDK, you might '
+            . 'receive a "quota exceeded" or "API not enabled" error. For '
+            . 'more information about service accounts, see '
+            . 'https://cloud.google.com/docs/authentication/.',
+            E_USER_WARNING
+        );
     }
 
     /**
