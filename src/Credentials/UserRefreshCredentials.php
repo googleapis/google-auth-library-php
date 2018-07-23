d9aa34be4003738f08fe31118b55e4618d2def0c
Takashi Matsuo <tmatsuo@google.com>
2018-07-23 13:46:13 -0700

    Indentation

--- src/Credentials/UserRefreshCredentials.php
+++ src/Credentials/UserRefreshCredentials.php
@@ -89,16 +89,16 @@ class UserRefreshCredentials extends CredentialsLoader
             && getenv(self::SUPPRESS_CLOUD_SDK_CREDS_WARNING_ENV) !== 'true') {
             trigger_error(
                 'Your application has authenticated using end user credentials '
-                    . 'from Gooogle Cloud SDK. We recommend that most server '
-                    . 'applications use service accounts instead. If your '
-                    . 'application continues to use end user credentials '
-                    . 'from Cloud SDK, you might receive a "quota exceeded" '
-                    . 'or "API not enabled" error. For more information about '
-                    . 'service accounts, see '
-                    . 'https://cloud.google.com/docs/authentication/. '
-                    . 'To disable this warning, set '
-                    . self::SUPPRESS_CLOUD_SDK_CREDS_WARNING_ENV
-                    . ' environment variable to "true".',
+                . 'from Gooogle Cloud SDK. We recommend that most server '
+                . 'applications use service accounts instead. If your '
+                . 'application continues to use end user credentials '
+                . 'from Cloud SDK, you might receive a "quota exceeded" '
+                . 'or "API not enabled" error. For more information about '
+                . 'service accounts, see '
+                . 'https://cloud.google.com/docs/authentication/. '
+                . 'To disable this warning, set '
+                . self::SUPPRESS_CLOUD_SDK_CREDS_WARNING_ENV
+                . ' environment variable to "true".',
                 E_USER_WARNING);
         }
     }
