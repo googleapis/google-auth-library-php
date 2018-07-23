1591bbadd1df6f266626beafaa996ec1c5852062
Takashi Matsuo <tmatsuo@google.com>
2018-07-23 14:09:33 -0700

    Removed unnecessary string concatinations

--- tests/Credentials/UserRefreshCredentialsTest.php
+++ tests/Credentials/UserRefreshCredentialsTest.php
@@ -99,7 +99,7 @@ class URCConstructorTest extends TestCase
      */
     public function testFailsToInitalizeFromANonExistentFile()
     {
-        $keyFile = __DIR__ . '/../fixtures' . '/does-not-exist-private.json';
+        $keyFile = __DIR__ . '/../fixtures/does-not-exist-private.json';
         new UserRefreshCredentials('scope/1', $keyFile);
     }
 
@@ -117,7 +117,7 @@ class URCConstructorTest extends TestCase
     public function testGcloudWarning()
     {
         putenv('SUPPRESS_GCLOUD_CREDS_WARNING=false');
-        $keyFile = __DIR__ . '/../fixtures2' . '/gcloud.json';
+        $keyFile = __DIR__ . '/../fixtures2/gcloud.json';
         $this->assertNotNull(
             new UserRefreshCredentials('scope/1', $keyFile)
         );
@@ -126,7 +126,7 @@ class URCConstructorTest extends TestCase
     public function testValid3LOauthCreds()
     {
         putenv('SUPPRESS_GCLOUD_CREDS_WARNING=false');
-        $keyFile = __DIR__ . '/../fixtures2' . '/valid_oauth_creds.json';
+        $keyFile = __DIR__ . '/../fixtures2/valid_oauth_creds.json';
         $this->assertNotNull(
             new UserRefreshCredentials('scope/1', $keyFile)
         );
@@ -150,14 +150,14 @@ class URCFromEnvTest extends TestCase
      */
     public function testFailsIfEnvSpecifiesNonExistentFile()
     {
-        $keyFile = __DIR__ . '/../fixtures' . '/does-not-exist-private.json';
+        $keyFile = __DIR__ . '/../fixtures/does-not-exist-private.json';
         putenv(UserRefreshCredentials::ENV_VAR . '=' . $keyFile);
         UserRefreshCredentials::fromEnv('a scope');
     }
 
     public function testSucceedIfFileExists()
     {
-        $keyFile = __DIR__ . '/../fixtures2' . '/private.json';
+        $keyFile = __DIR__ . '/../fixtures2/private.json';
         putenv(UserRefreshCredentials::ENV_VAR . '=' . $keyFile);
         $this->assertNotNull(ApplicationDefaultCredentials::getCredentials('a scope'));
     }
