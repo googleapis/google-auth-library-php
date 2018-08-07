333bc6c4d626b62acba77d236494ada6f8ce25b3
Jeff Ching <chingor@google.com>
2018-08-07 14:05:05 -0700

    Update token url

--- src/CredentialsLoader.php
+++ src/CredentialsLoader.php
@@ -26,7 +26,7 @@ use Google\Auth\Credentials\UserRefreshCredentials;
  */
 abstract class CredentialsLoader implements FetchAuthTokenInterface
 {
-    const TOKEN_CREDENTIAL_URI = 'https://www.googleapis.com/oauth2/v4/token';
+    const TOKEN_CREDENTIAL_URI = 'https://oauth2.googleapis.com/token';
     const ENV_VAR = 'GOOGLE_APPLICATION_CREDENTIALS';
     const WELL_KNOWN_PATH = 'gcloud/application_default_credentials.json';
     const NON_WINDOWS_WELL_KNOWN_PATH_BASE = '.config';
