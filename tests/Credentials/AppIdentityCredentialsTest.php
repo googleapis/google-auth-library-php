6122896969b06745a117aa47a0502fdb7ca024fb
Jérôme Gamez <jerome@gamez.name>
2020-01-11 23:56:32 +0100

    fix: let namespace match the file structure (#258)
    The test suite is file based so this didn't throw an error, but since
    the classes in the tests folder are referenced in the composer.json
    in the autoload-dev section, we can prevent problems in the future.

--- tests/Credentials/AppIdentityCredentialsTest.php
+++ tests/Credentials/AppIdentityCredentialsTest.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-namespace Google\Auth\Tests\Cache;
+namespace Google\Auth\Tests\Credentials;
 
 use google\appengine\api\app_identity\AppIdentityService;
 // included from tests\mocks\AppIdentityService.php
--- tests/Credentials/InsecureCredentialsTest.php
+++ tests/Credentials/InsecureCredentialsTest.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-namespace Google\Auth\Tests\Cache;
+namespace Google\Auth\Tests\Credentials;
 
 use Google\Auth\Credentials\InsecureCredentials;
 use PHPUnit\Framework\TestCase;
