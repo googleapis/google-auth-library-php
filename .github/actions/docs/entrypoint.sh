5647b71bd7ac906e490d65c5af22f4c5a2591a81
John Pedrie <john@pedrie.com>
2020-03-31 16:06:38 -0400

    chore: generate and push documentation

---
+++ .github/actions/docs/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -l
+
+apt-get update
+apt-get install -y git
+
+curl -O http://get.sensiolabs.org/sami.phar
+
+git reset HEAD .docs
+git reset HEAD .gitmodules
+
+php sami.phar update .github/actions/docs/sami.php
+
+cd ./.docs
+
+git config user.name "GitHub Actions"
+git config user.email "actions@github.com"
+
+git add .
+git commit -m "Updating docs"
+git push -q https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY} HEAD:gh-pages
