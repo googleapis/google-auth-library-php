8cf5cbb04197bfc507174cd154924132b4427275
John Pedrie <john@pedrie.com>
2020-04-14 12:49:38 -0400

    always generate from scratch

--- .github/actions/docs/entrypoint.sh
+++ .github/actions/docs/entrypoint.sh
@@ -5,17 +5,16 @@ apt-get install -y git
 
 curl -O http://get.sensiolabs.org/sami.phar
 
-git reset HEAD .docs
-git reset HEAD .gitmodules
 git reset --hard HEAD
 
 php vendor/bin/sami.php update .github/actions/docs/sami.php
 
 cd ./.docs
 
+git init
 git config user.name "GitHub Actions"
 git config user.email "actions@github.com"
 
 git add .
 git commit -m "Updating docs"
-git push -q https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY} HEAD:gh-pages
+git push -q https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/${GITHUB_REPOSITORY} HEAD:gh-pages --force
