caa877d571acea24b3bc2bb8fdfb6837719bdbaf
Brent Shaffer <bshafs@gmail.com>
2020-10-02 22:38:10 -0700

    tweak to OAuth2::getLastReceivedToken

--- src/OAuth2.php
+++ src/OAuth2.php
@@ -1303,36 +1303,33 @@ class OAuth2 implements FetchAuthTokenInterface
     /**
      * The expiration of the last received token.
      *
-     * @return array
+     * @return array|null
      */
     public function getLastReceivedToken()
     {
-        $authToken = null;
         if ($token = $this->getAccessToken()) {
             // the bare necessity of an auth token
             $authToken = [
                 'access_token' => $token,
                 'expires_at' => $this->getExpiresAt(),
             ];
-        }
-
-        if ($idToken = $this->getIdToken()) {
+        } elseif ($idToken = $this->getIdToken()) {
             $authToken = [
                 'id_token' => $idToken,
                 'expires_at' => $this->getExpiresAt(),
             ];
+        } else {
+            return null;
         }
 
-        if ($authToken) {
-            if ($expiresIn = $this->getExpiresIn()) {
-                $authToken['expires_in'] = $expiresIn;
-            }
-            if ($issuedAt = $this->getIssuedAt()) {
-                $authToken['issued_at'] = $issuedAt;
-            }
-            if ($refreshToken = $this->getRefreshToken()) {
-                $authToken['refresh_token'] = $refreshToken;
-            }
+        if ($expiresIn = $this->getExpiresIn()) {
+            $authToken['expires_in'] = $expiresIn;
+        }
+        if ($issuedAt = $this->getIssuedAt()) {
+            $authToken['issued_at'] = $issuedAt;
+        }
+        if ($refreshToken = $this->getRefreshToken()) {
+            $authToken['refresh_token'] = $refreshToken;
         }
 
         return $authToken;
