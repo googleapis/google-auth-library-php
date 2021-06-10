3cd08086756ffc715fe5cd048c7eb5c87685bce2
Brent Shaffer <betterbrent@google.com>
2021-06-10 12:02:44 -0700

    Update FetchAuthTokenCache.php

--- src/FetchAuthTokenCache.php
+++ src/FetchAuthTokenCache.php
@@ -142,7 +142,8 @@ class FetchAuthTokenCache implements
             );
         }
 
-        // Fetch access token from cache for signing a blob
+        // Pass the access token from cache to GCECredentials for signing a blob.
+        // This saves a call to the metadata server when a cached token exists.
         if ($this->fetcher instanceof Credentials\GCECredentials) {
             $cached = $this->fetchAuthTokenFromCache();
             $accessToken = isset($cached['access_token']) ? $cached['access_token'] : null;
