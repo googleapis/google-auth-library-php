865efaa1265ac5015056419f895f4166b482c731
ShaRy <61742181+SharyRajpoot@users.noreply.github.com>
2022-04-21 11:36:55 +0500

    Laravel .env update
    Added function to get `GOOGLE_APPLICATION_CREDENTIALS` from `.env` file of the Laravel project.
    For this user needs to add ` GOOGLE_APPLICATION_CREDENTIALS`  in `.env` file of laravel and sets the key `google_application_credentials` in `config\app.php`  with value pointing to .env of laravel.
    e:g.
    
    1 .env
    ```
    .....
    'GOOGLE_APPLICATION_CREDENTIALS=/path/to/my/credentials.json'
    ```
    
    2. config\app.php
    ```
    .....
    'google_application_credentials' => env('GOOGLE_APPLICATION_CREDENTIALS', null),
    ```

--- src/CredentialsLoader.php
+++ src/CredentialsLoader.php
@@ -70,7 +70,7 @@ abstract class CredentialsLoader implements
      */
     public static function fromEnv()
     {
-        $path = getenv(self::ENV_VAR);
+        $path = getenv(self::ENV_VAR) ?: config('app.google_application_credentials');
         if (empty($path)) {
             return null;
         }
