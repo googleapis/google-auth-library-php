cf7ac54454bbb8ad6d12c652c05f5d7b5eb2d701
Brent Shaffer <betterbrent@google.com>
2023-01-26 13:04:09 -0800

    feat: add getFetcher to FetchAuthTokenCache (#431)

--- tests/FetchAuthTokenCacheTest.php
+++ tests/FetchAuthTokenCacheTest.php
@@ -599,4 +599,17 @@ class FetchAuthTokenCacheTest extends BaseTest
 
         $fetcher->getProjectId();
     }
+
+    public function testGetFetcher()
+    {
+        $mockFetcher = $this->prophesize('Google\Auth\FetchAuthTokenInterface')
+            ->reveal();
+        $fetcher = new FetchAuthTokenCache(
+            $mockFetcher,
+            [],
+            $this->mockCache->reveal()
+        );
+
+        $this->assertSame($mockFetcher, $fetcher->getFetcher());
+    }
 }
