03a72d7f6cc4c090c7a13010a515defc9cb5fbbc
Damien AVETTA-RAYMOND <damien.avetta@verdie.com>
2023-05-16 12:31:32 +0200

    replace getenv by $_ENV variable for call GOOGLE_APPLICATION_CREDENTIALS env variable

--- src/CredentialsLoader.php
+++ src/CredentialsLoader.php
@@ -72,7 +72,7 @@ abstract class CredentialsLoader implements
      */
     public static function fromEnv()
     {
-        $path = getenv(self::ENV_VAR);
+        $path = $_ENV[self::ENV_VAR];
         if (empty($path)) {
             return null;
         }
