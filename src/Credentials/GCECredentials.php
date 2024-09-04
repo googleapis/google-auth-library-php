61c7ecb552bd3e7372c447538521300d12819a22
Brent Shaffer <betterbrent@google.com>
2024-09-04 12:57:22 -0700

    split lint workflow, update cs

--- src/Credentials/GCECredentials.php
+++ src/Credentials/GCECredentials.php
@@ -426,12 +426,12 @@ class GCECredentials extends CredentialsLoader implements
 
         try {
             $productName = $shell->regRead($registryProductKey);
-        } catch(com_exception) {
+        } catch (com_exception) {
             // This means that we tried to read a key that doesn't exist on the registry
             // which might mean that it is a windows instance that is not on GCE
             return false;
         }
-        
+
         return 0 === strpos($productName, self::PRODUCT_NAME);
     }
 
