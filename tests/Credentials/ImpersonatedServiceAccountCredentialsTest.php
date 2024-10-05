19ae60866454fa562c893049258a3ab3e8454564
Brent Shaffer <betterbrent@google.com>
2024-10-04 18:52:47 -0700

    add test for Impersonating ExternalAccountCreds

--- tests/Credentials/ImpersonatedServiceAccountCredentialsTest.php
+++ tests/Credentials/ImpersonatedServiceAccountCredentialsTest.php
@@ -18,6 +18,7 @@
 
 namespace Google\Auth\Tests\Credentials;
 
+use Google\Auth\Credentials\ExternalAccountCredentials;
 use Google\Auth\Credentials\ImpersonatedServiceAccountCredentials;
 use Google\Auth\Credentials\ServiceAccountCredentials;
 use Google\Auth\Credentials\UserRefreshCredentials;
@@ -39,18 +40,19 @@ class ImpersonatedServiceAccountCredentialsTest extends TestCase
 
     private const SCOPE = ['scope/1', 'scope/2'];
     private const TARGET_AUDIENCE = 'test-target-audience';
+    private const IMPERSONATION_URL = 'https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/test@test-project.iam.gserviceaccount.com:generateToken';
 
     public function testGetServiceAccountNameEmail()
     {
-        $json = $this->userToServiceAccountImpersonationJson;
+        $json = self::USER_TO_SERVICE_ACCOUNT_JSON;
         $creds = new ImpersonatedServiceAccountCredentials(self::SCOPE, $json);
         $this->assertEquals('test@test-project.iam.gserviceaccount.com', $creds->getClientName());
     }
 
     public function testGetServiceAccountNameID()
     {
-        $json = $this->userToServiceAccountImpersonationJson;
-        $json['service_account_impersonation_url'] = 'https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/1234567890987654321:generateAccessToken';
+        $json = self::USER_TO_SERVICE_ACCOUNT_JSON;
+        $json['service_account_impersonation_url'] = 'https://some/arbitrary/url/1234567890987654321:generateAccessToken';
         $creds = new ImpersonatedServiceAccountCredentials(self::SCOPE, $json);
         $this->assertEquals('1234567890987654321', $creds->getClientName());
     }
@@ -89,8 +91,8 @@ class ImpersonatedServiceAccountCredentialsTest extends TestCase
     public function provideServiceAccountImpersonationJson()
     {
         return [
-            [$this->userToServiceAccountImpersonationJson, UserRefreshCredentials::class],
-            [$this->serviceAccountToServiceAccountImpersonationJson, ServiceAccountCredentials::class],
+            [self::USER_TO_SERVICE_ACCOUNT_JSON, UserRefreshCredentials::class],
+            [self::SERVICE_ACCOUNT_TO_SERVICE_ACCOUNT_JSON, ServiceAccountCredentials::class],
         ];
     }
 
@@ -129,6 +131,42 @@ class ImpersonatedServiceAccountCredentialsTest extends TestCase
         $token = $creds->fetchAuthToken($httpHandler);
         $this->assertEquals('test-impersonated-id-token', $token['id_token']);
         $this->assertEquals(2, $requestCount);
+    }
+
+    public function testGetIdTokenWithExternalAccountToServiceAccountImpersonationCredentials()
+    {
+        $json = self::EXTERNAL_ACCOUNT_TO_SERVICE_ACCOUNT_JSON;
+        $httpHandler = function (RequestInterface $request) use (&$requestCount, $json) {
+            if (++$requestCount == 1) {
+                // the call to swap the refresh token for an access token
+                $this->assertEquals(
+                    $json['source_credentials']['credential_source']['url'],
+                    (string) $request->getUri()
+                );
+            } elseif ($requestCount == 2) {
+                $this->assertEquals($json['source_credentials']['token_url'], (string) $request->getUri());
+            } elseif ($requestCount == 3) {
+                // the call to swap the access token for an id token
+                $this->assertEquals($json['service_account_impersonation_url'], (string) $request->getUri());
+                $this->assertEquals(self::TARGET_AUDIENCE, json_decode($request->getBody(), true)['audience'] ?? '');
+                $this->assertEquals('Bearer test-access-token', $request->getHeader('authorization')[0] ?? null);
+            }
+
+            return new Response(
+                200,
+                ['Content-Type' => 'application/json'],
+                json_encode(match ($requestCount) {
+                    1 => ['access_token' => 'test-access-token'],
+                    2 => ['access_token' => 'test-access-token'],
+                    3 => ['token' => 'test-impersonated-id-token']
+                })
+            );
+        };
+
+        $creds = new ImpersonatedServiceAccountCredentials(null, $json, self::TARGET_AUDIENCE);
+        $token = $creds->fetchAuthToken($httpHandler);
+        $this->assertEquals('test-impersonated-id-token', $token['id_token']);
+        $this->assertEquals(3, $requestCount);
     }
 
     public function testGetIdTokenWithArbitraryCredentials()
@@ -186,16 +224,15 @@ class ImpersonatedServiceAccountCredentialsTest extends TestCase
     public function provideServiceAccountImpersonationIdTokenJson()
     {
         return [
-            [$this->userToServiceAccountImpersonationIdTokenJson, 'refresh_token'],
-            [$this->serviceAccountToServiceAccountImpersonationIdTokenJson, OAuth2::JWT_URN],
+            [self::USER_TO_SERVICE_ACCOUNT_JSON, 'refresh_token'],
+            [self::SERVICE_ACCOUNT_TO_SERVICE_ACCOUNT_JSON, OAuth2::JWT_URN],
         ];
     }
 
     public function testIdTokenWithAuthTokenMiddleware()
     {
         $targetAudience = 'test-target-audience';
-        $json = $this->userToServiceAccountImpersonationIdTokenJson;
-        $credentials = new ImpersonatedServiceAccountCredentials(null, $json, $targetAudience);
+        $credentials = new ImpersonatedServiceAccountCredentials(null, self::USER_TO_SERVICE_ACCOUNT_JSON, $targetAudience);
 
         // this handler is for the middleware constructor, which will pass it to the ISAC to fetch tokens
         $httpHandler = getHandler([
@@ -221,9 +258,9 @@ class ImpersonatedServiceAccountCredentialsTest extends TestCase
     }
 
     // User Refresh to Service Account Impersonation JSON Credentials
-    private array $userToServiceAccountImpersonationJson = [
+    private const USER_TO_SERVICE_ACCOUNT_JSON = [
         'type' => 'impersonated_service_account',
-        'service_account_impersonation_url' => 'https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/test@test-project.iam.gserviceaccount.com:generateAccessToken',
+        'service_account_impersonation_url' => self::IMPERSONATION_URL,
         'source_credentials' => [
             'client_id' => 'client123',
             'client_secret' => 'clientSecret123',
@@ -233,40 +270,28 @@ class ImpersonatedServiceAccountCredentialsTest extends TestCase
     ];
 
     // Service Account to Service Account Impersonation JSON Credentials
-    private array $serviceAccountToServiceAccountImpersonationJson = [
+    private const SERVICE_ACCOUNT_TO_SERVICE_ACCOUNT_JSON = [
         'type' => 'impersonated_service_account',
-        'service_account_impersonation_url' => 'https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/test@test-project.iam.gserviceaccount.com:generateAccessToken',
-        'source_credentials' => [
-            'client_email' => 'clientemail@clientemail.com',
-            'private_key' => 'privatekey123',
-            'type' => 'service_account',
-        ]
-    ];
-
-    // User Refresh to Service Account Impersonation ID Token JSON Credentials
-    // NOTE: The only difference is the use of "generateIdToken" instead of
-    // "generateAccessToken" in the service_account_impersonation_url
-    private array $userToServiceAccountImpersonationIdTokenJson = [
-        'type' => 'impersonated_service_account',
-        'service_account_impersonation_url' => 'https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/test@test-project.iam.gserviceaccount.com:generateIdToken',
-        'source_credentials' => [
-            'client_id' => 'client123',
-            'client_secret' => 'clientSecret123',
-            'refresh_token' => 'refreshToken123',
-            'type' => 'authorized_user',
-        ]
-    ];
-
-    // Service Account to Service Account Impersonation ID Token JSON Credentials
-    // NOTE: The only difference is the use of "generateIdToken" instead of
-    // "generateAccessToken" in the service_account_impersonation_url
-    private array $serviceAccountToServiceAccountImpersonationIdTokenJson = [
-        'type' => 'impersonated_service_account',
-        'service_account_impersonation_url' => 'https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/test@test-project.iam.gserviceaccount.com:generateIdToken',
+        'service_account_impersonation_url' => self::IMPERSONATION_URL,
         'source_credentials' => [
             'client_email' => 'clientemail@clientemail.com',
             'private_key' => "-----BEGIN RSA PRIVATE KEY-----\nMIICWgIBAAKBgGhw1WMos5gp2YjV7+fNwXN1tI4/DFXKzwY6TDWsPxkbyfjHgunX\n/sijlnJt3Qs1gBxiwEEjzFFlp39O3/gEbIoYWHR/4sZdqNRFzbhJcTpnUvRlZDBL\nE5h8f5uu4aL4D32WyiELF/vpr533lZCBwWsnN3zIYJxThgRF9i/R7F8tAgMBAAEC\ngYAgUyv4cNSFOA64J18FY82IKtojXKg4tXi1+L01r4YoA03TzgxazBtzhg4+hHpx\nybFJF9dhUe8fElNxN7xiSxw8i5MnfPl+piwbfoENhgrzU0/N14AV/4Pq+WAJQe2M\nxPcI1DPYMEwGjX2PmxqnkC47MyR9agX21YZVc9rpRCgPgQJBALodH492I0ydvEUs\ngT+3DkNqoWx3O3vut7a0+6k+RkM1Yu+hGI8RQDCGwcGhQlOpqJkYGsVegZbxT+AF\nvvIFrIUCQQCPqJbRalHK/QnVj4uovj6JvjTkqFSugfztB4Zm/BPT2eEpjLt+851d\nIJ4brK/HVkQT2zk9eb0YzIBfeQi9WpyJAkB9+BRSf72or+KsV1EsFPScgOG9jn4+\nhfbmvVzQ0ouwFcRfOQRsYVq2/Z7LNiC0i9LHvF7yU+MWjUJo+LqjCWAZAkBHearo\nMIzXgQRGlC/5WgZFhDRO3A2d8aDE0eymCp9W1V24zYNwC4dtEVB5Fncyp5Ihiv40\nvwA9eWoZll+pzo55AkBMMdk95skWeaRv8T0G1duv5VQ7q4us2S2TKbEbC8j83BTP\nNefc3KEugylyAjx24ydxARZXznPi1SFeYVx1KCMZ\n-----END RSA PRIVATE KEY-----\n",
             'type' => 'service_account',
+        ]
+    ];
+
+    // Service Account to Service Account Impersonation JSON Credentials
+    private const EXTERNAL_ACCOUNT_TO_SERVICE_ACCOUNT_JSON = [
+        'type' => 'impersonated_service_account',
+        'service_account_impersonation_url' => self::IMPERSONATION_URL,
+        'source_credentials' => [
+            'type' => 'external_account',
+            'audience' => 'some_audience',
+            'subject_token_type' => 'access_token',
+            'token_url' => 'https://sts.googleapis.com/v1/token',
+            'credential_source' => [
+                'url' => 'https://some.url/token'
+            ]
         ]
     ];
 }
